252cfbdd14b113fae1083e647a7794e87e566c42
yarnell <mryarnell@foxmail.com>
2017-07-19 13:20:36 +0800

    第1版：简单的创建了一个Hello world程序

---
+++ hello_world.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  hello_world.c
+ *
+ *    Description:  这是一个hello world程序
+ *
+ *        Version:  1.0
+ *        Created:  2017年07月19日 11时45分10秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (陶逸南), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    printf("hello world\n");
+    return 0;
+}
